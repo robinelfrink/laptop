ff5e6c0270a5510341398119ce8ea3d4d8331cb1
Robin Elfrink <robin@15augustus.nl>
2025-02-19 13:22:58 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -6,28 +6,23 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelModules = ["ecryptfs"];
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # Encryption
+  security.pam.enableEcryptfs = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  # Networking
+  networking.hostName = "zaphod";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/Amsterdam";
-
-  # Select internationalisation properties.
+  # Localization
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -68,16 +63,9 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # This is me.
   users.users.robin = {
     isNormalUser = true;
     description = "Robin Elfrink";
@@ -86,7 +74,6 @@
     #  thunderbird
     ];
     hashedPassword = "$6$1ibk.hBqsHrevy5x$naYYCBfe8Y9rCOFRga/L8dfZbJHoTkWhsQ1ol0KzfG91vY8RTBXJrrFo.TDXbZtjtIVFnblawSeu6bvKGmcmb/";
-    openssh.authorizedKeys.keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCixA3AzLT0Ado3CgKafmVIa3E4ybPCxVBw45wugo1zmcjMdWNAAmSkE0KvUe0l9d0OcjyjCdMZH8gV/bOAAyk4dVA943nMt+YmXR7bY4SKm2Uz5CWypwVHR1HG1KGBYCFnpGRBiOROmz3AchgDqnbGcR6F6W5bf80THOT5VtPb1wGbn/TchllnTOaa9UwXYqC+sdODGMccBKm5YIVZr7ciJH6wa51YWsP+VbGaZjcGsUdQ+p4m1Xzmgled2/WmtJPdhunB2kw8mS89WRUKKlrptSJ3gBd8wcv8QttQn/X/QbhRsv2jKj66Opz1w9aeZiO/Ra1GFXwYEEKJzPKjaY/l"];
   };
 
   # Install firefox.
@@ -98,36 +85,11 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     git
-  #  wget
+    ecryptfs
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.11";
 
 }
